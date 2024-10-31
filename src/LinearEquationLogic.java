fef651b6fc1dcc585c7244cd1903813a699c7cde
BT_1N3_12 <sampleemail@gmail.com>
2024-10-31 10:23:11 -0400

    Implementd the vertical line method into the game loop.

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -7,7 +7,8 @@ public class LinearEquationLogic {
     private String coordinate;
     private String coordinate2;
     private LinearEquation line;
-
+    private int x1Int;
+    private int x2Int;
 
     //constructor
     public LinearEquationLogic() {
@@ -15,6 +16,8 @@ public class LinearEquationLogic {
         coordinate2 = null;
         line = null;
         scan = new Scanner(System.in);
+        x1Int = 0;
+        x2Int = 0;
     }
 
 
@@ -36,14 +39,14 @@ public class LinearEquationLogic {
         int comma1 = coordinate.indexOf(",");
         String x1Value = coordinate.substring(1,comma1);
         String y1Value = coordinate.substring(comma1 + 1, coordinate.length()-1);
-        int x1Int = Integer.parseInt(x1Value);
+        x1Int = Integer.parseInt(x1Value);
         int y1Int = Integer.parseInt(y1Value);
 
 
         int comma2 = coordinate2.indexOf(",");
         String x2Value = coordinate2.substring(1,comma2);
         String y2Value = coordinate2.substring(comma2 + 1,coordinate2.length()-1);
-        int x2Int = Integer.parseInt(x2Value);
+        x2Int = Integer.parseInt(x2Value);
         int y2Int = Integer.parseInt(y2Value);
 
 
@@ -69,23 +72,29 @@ public class LinearEquationLogic {
     private void gameLoop () {
         getCoordinateInfo();
         parseCoordinate();
-        checkVertical();
-        printLineInfo();
-        coordinateWithX();
-        System.out.print("Do you want to continue doing this process (yes/no): ");
-        String condition = scan.nextLine();
-        while (condition.equals("yes")) {
-            getCoordinateInfo();
-            parseCoordinate();
+        if (x1Int != x2Int) {
             printLineInfo();
             coordinateWithX();
-            System.out.println("Do you want to continue doing this process (yes/no): ");
-            condition = scan.nextLine();
+            System.out.print("Do you want to continue doing this process (yes/no): ");
+            String condition = scan.nextLine();
+            while (condition.equals("yes") && x1Int != x2Int) {
+                getCoordinateInfo();
+                parseCoordinate();
+                printLineInfo();
+                coordinateWithX();
+                System.out.println("Do you want to continue doing this process (yes/no): ");
+                condition = scan.nextLine();
+            }
+        } else {
+            checkVertical();
         }
     }
 
     private void checkVertical() {
-
+        if (x1Int == x2Int) {
+            System.out.println("Your line is vertical.");
+            System.out.println("Its equation is x = " + x1Int);
+        }
 
     }
 }
